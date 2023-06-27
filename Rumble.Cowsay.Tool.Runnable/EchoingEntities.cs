d4b9d70dff6e162eb18ad1ba0f3bfcb884b83626
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2023-06-27 15:24:21 +0300

    Changed library usage logic

---
+++ Rumble.Cowsay.Tool.Runnable/EchoingEntities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Rumble.Cowsay.Tool.Runnable;
+
+/// <summary>
+/// Predefined echoing entities.
+/// </summary>
+internal static class EchoingEntities
+{
+	internal static EchoEntity Cow => new ()
+	{
+		DefaultPhrase = "Moo!",
+		Appearance =
+		(
+			"""^__^"""             + Environment.NewLine +
+			"""(oo)\_______"""     + Environment.NewLine +
+			"""(__)\       )\/\""" + Environment.NewLine +
+			"""    ||----W |"""    + Environment.NewLine +
+			"""    ||     ||"""
+		)
+	};
+
+	internal static EchoEntity Elephant => new ()
+	{
+		DefaultPhrase = "Pawoo!",
+		Appearance =
+		(
+			"""  __     __"""           + Environment.NewLine +
+			""" /  \~~~/  \"""          + Environment.NewLine +
+			"""(    ..     )----."""    + Environment.NewLine +
+			""" \__     __/      \"""   + Environment.NewLine +
+			"""   )|  /)         |\"""  + Environment.NewLine +
+			"""    | /\  /___\   / ^""" + Environment.NewLine +
+			"""     "-|__|   |__|"""
+		)
+	};
+}
