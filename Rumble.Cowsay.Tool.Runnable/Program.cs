2d27c64289662d0e8bd81d9d3f593cc507482181
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2023-06-27 20:10:36 +0300

    Added entityName option

--- Rumble.Cowsay.Tool.Runnable/Program.cs
+++ Rumble.Cowsay.Tool.Runnable/Program.cs
@@ -1,33 +1,50 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using Cocona;
+using Humanizer;
 using Rumble.Cowsay.Tool.Runnable;
 using Rumble.Essentials;
-using Serilog;
+// using Serilog;
+
+// ReSharper disable MissingIndent
 
 Console.InputEncoding = Encoding.UTF8;
 Console.OutputEncoding = Encoding.UTF8;
 
-Log.Logger = Essential.OfType<ILogger>();
-CoconaApp.Run((string? phrase, int? lineLength) =>
+// Log.Logger = Essential.OfType<ILogger>();
+CoconaApp.Run((string? entityName, string? phrase, int? lineLength) =>
 {
-	var logger = Log.Logger.ForContext<Program>();
-	logger.Information("Application has been started");
+	// var logger = Log.Logger.ForContext<Program>();
+	// logger.Information("Application has been started");
 
-	var repeatingEntity = EchoingEntities.Cow;
+	var existingEntities = new []
+	{
+		EchoingEntities.Cow,
+		EchoingEntities.Elephant
+	};
+
+	var entity =
+	(
+		entityName is not null &&
+		existingEntities.SingleOrDefault(entity => entity.Name.Equals(entityName.Kebaberize())) is { } entry
+	)
+	? entry
+	: existingEntities.First();
+
 	Console.WriteLine
 	(
 		(phrase, lineLength) switch
 		{
-			{ phrase: not null, lineLength: not null } => repeatingEntity.Echo(phrase, lineLength.Value),
-			{ phrase: not null } => repeatingEntity.Echo(phrase),
-			_ => repeatingEntity.Speak()
+			{ phrase: not null, lineLength: not null } => entity.Echo(phrase, lineLength.Value),
+			{ phrase: not null } => entity.Echo(phrase),
+			_ => entity.Speak()
 		}
 	);
 
-	logger.Information("Application has been shut down");
-	logger.Information("");
+	// logger.Information("Application has been shut down");
+	// logger.Information("");
 });
 
-Log.CloseAndFlush();
+// Log.CloseAndFlush();
 Environment.Exit(EnvironmentExitCode.Success);
