0ae4ed1751ba73187eaca30da6a4bde841dee3c6
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2023-06-27 15:23:05 +0300

    Added predefined symbols to avoid raw literals

---
+++ Rumble.Cowsay/Symbol.cs
@@ -0,0 +1,22 @@
+﻿namespace Rumble.Cowsay;
+
+/// <summary>
+/// Predefined symbols.
+/// </summary>
+public static class Symbol
+{
+	/// <summary>
+	/// Space symbol.
+	/// </summary>
+	public static char Space => ' ';
+
+	/// <summary>
+	/// Underscore symbol.
+	/// </summary>
+	public static char Underscore => '_';
+
+	/// <summary>
+	/// Underscore symbol.
+	/// </summary>
+	public new static char Equals => '=';
+}
