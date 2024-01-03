6ca5c71699de915a3779183f5bcbd2fe6b3c2f0b
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2024-01-03 14:37:19 +0200

    Updated codestyle

--- Rumrunner0.Cowsay.Tool.Runnable/Program.cs
+++ Rumrunner0.Cowsay.Tool.Runnable/Program.cs
@@ -10,16 +10,13 @@ Console.OutputEncoding = Encoding.UTF8;
 
 CoconaApp.Run((string? entityName, string? phrase, int? lineLength) =>
 {
-	var entities = (EchoEntity[]) [EchoingEntity.Cow, EchoingEntity.Elephant];
+	var entities = (EchoEntity[])[ EchoingEntity.Cow, EchoingEntity.Elephant ];
 	var entity = entities.FirstOrDefault(e => e.Name.Equals(entityName?.Kebaberize())) ?? entities.First();
 
-	Console.WriteLine
-	(
-		(phrase, lineLength) switch
-		{
-			{ phrase: not null, lineLength: not null } => entity.Echo(phrase, lineLength.Value),
-			{ phrase: not null } => entity.Echo(phrase),
-			_ => entity.Speak()
-		}
-	);
+	Console.WriteLine((phrase, lineLength) switch
+	{
+		{ phrase: not null, lineLength: not null } => entity.Echo(phrase, lineLength.Value),
+		{ phrase: not null } => entity.Echo(phrase),
+		_ => entity.Speak()
+	});
 });
